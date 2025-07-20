a6f6e218a3d17aa8f24f0a14f152de3f1da33a2f
Maulanatazqi <maulanatazqi@gmail.com>
2025-07-20 15:17:50 +0700

    bruh moment: missing changes from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CarouselView from "@/components/carousel/CarouselView";
 import { FadeUp } from "@/components/Motions/FadeUp";
 
 import Intro from "@/components/Sections/[1]Intro/Intro";
@@ -11,17 +12,21 @@ export default function Home() {
     <>
       {/* <NavStateController backButton={true} /> */}
       <div className="pointer-events-none fixed inset-0 z-0 h-screen w-full bg-noise bg-[size:180px] bg-repeat opacity-[0.025]" />
-      <main className="relative flex min-h-screen w-full max-w-[420px] flex-col transition-all duration-300 md:max-w-[580px]">
-        <div className="pointer-events-none fixed z-0 h-screen w-full max-w-[420px] md:max-w-[580px]" />
-        <div className="z-10 mt-[40px] w-full rounded-[28px]">
+      <main className="relative flex min-h-screen w-full flex-col transition-all duration-300">
+        <div className="pointer-events-none fixed z-0 h-screen w-full" />
+        <div className="z-10 w-full rounded-[28px]">
           {/* <div className="relative flex h-[200px] w-full overflow-hidden">
             <div className="absolute inset-0 h-full w-full bg-[radial-gradient(#E3E8EF_1px,transparent_1px)] [background-size:18px_18px]"></div>
           </div> */}
           {/* <NavStateController backButton={false} loading={false} /> */}
-          <FadeUp delay={0} duration={0.8} className="flex w-full flex-col">
+          <FadeUp
+            delay={0}
+            duration={0.8}
+            className="flex w-full flex-col items-center"
+          >
             <Intro />
             <About />
-
+            <CarouselView />
             <Writings icon={false} />
 
             <OtherWork />
